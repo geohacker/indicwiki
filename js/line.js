deed6e69660e2e7988c760f350e31396d2fb362b
Sajjad Anwar <sajjadkm@gmail.com>
2013-03-08 16:25:45 +0530

    Basic parameter line charts for every language

--- js/line.js
+++ js/line.js
@@ -1,9 +1,8 @@
-  var margin = {top: 20, right: 10, bottom: 20, left: 40},
+  var margin = {top: 40, right: 10, bottom: 20, left: 70},
   width = 1000 - margin.left - margin.right,
   height = 540 - margin.top - margin.bottom;
 
   var parseDate = d3.time.format("%d-%m-%Y").parse;
-  dataCirclesGroup = null;
 
   var x = d3.time.scale()
   .range([0, width]);
@@ -19,9 +18,23 @@
   .scale(y)
   .orient("left");
 
+  var data;
+  var flag = 0;
+
+  d3.csv("/indicwiki/data/data.csv", function(error, csv) {
+    data = csv;
+    initialize();
+    changeLanguage('hi');
+  });
+
+  function filterLanguage(code) {
+    return (function(d) {return (d.language == code);});
+  }
+
   var pointRadius = 4;
+  
   var attributes = ['active_editors', 'new_editors', 'total_editors',
-    'new_articles', 'total_articles'];
+  'new_articles', 'total_articles'];
 
     var languages = [{name:'Hindi', code:'hi'},
     {name:'Bengali', code:'bn'},
@@ -44,38 +57,40 @@
     {name: 'Sanskrit', code:'sa'},
     {name: 'Pali', code:'pi'}];
 
-var data, svg;
 
-function capitalise(string)
-{   
+  function capitalise(string)
+  {   
     split = string.split('_');
     return split[0].charAt(0).toUpperCase() + split[0].slice(1) + ' ' + split[1].charAt(0).toUpperCase() + split[1].slice(1);
+  }
+
+
+
+function clear () {
+  d3.selectAll('svg').remove();
 }
 
-d3.csv("/indicwiki/data/data.csv", function(error, csv) {data = csv;});
 
-function initialize() {
-  var hindi = data.filter(filterLanguage('hi'));
-    hindi.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.active_editors = +d.active_editors;
-    // console.log(d.date);
-  });
+function draw(data, attribute) {
+  console.log(attribute);
 
-  svg = d3.select('#active_editors').append("svg")
+
+  var dataCirclesGroup = null;
+
+  x.domain(d3.extent(data, function(d) { return d['date']; }));
+  y.domain(d3.extent(data, function(d) { return d[attribute]; }));
+
+  var svg = d3.select('#'+attribute).append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
+
   var line = d3.svg.line()
-  .x(function(d) { return x(d.date); })
-  .y(function(d) { return y(d.active_editors); });
+  .x(function(d) { return x(d['date']); })
+  .y(function(d) { return y(d[attribute]); });
 
-  x.domain(d3.extent(hindi, function(d) { return d.date; }));
-  y.domain(d3.extent(hindi, function(d) { return d.active_editors; }));
-
-    svg.append("g")
+  svg.append("g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + height + ")")
   .call(xAxis);
@@ -88,18 +103,18 @@ function initialize() {
   .attr("y", 6)
   .attr("dy", ".71em")
   .style("text-anchor", "end")
-  .text(capitalise('active_editors'));
+  .text(capitalise(attribute));
 
   svg.append("path")
-  .datum(hindi)
+  .datum(data)
   .attr("class", "line")
   .attr("d", line);
-  
+
   if (!dataCirclesGroup) {
     dataCirclesGroup = svg.append('svg:g');
   }
 
-  filtered = hindi.filter(function(d) { if (d.active_editors!=0){return d};});
+  filtered = data.filter(function(d) { if (d[attribute]!=0){return d};});
 
   var circles = dataCirclesGroup.selectAll('.data-point')
   .data(filtered);
@@ -109,146 +124,81 @@ function initialize() {
   .append('svg:circle')
   .attr('class', 'data-point')
   .style('opacity', 1e-6)
-  .attr('cx', function(d) { return x(d.date) })
+  .attr('cx', function(d) { return x(d['date']) })
   .attr('cy', function() { return y(0) })
   .attr('r', function() { return (2)})
-        // .attr('r', function() { return (hindi.length <= 50) ? pointRadius : 0 })
-        .transition()
-        .duration(2000)
-        .style('opacity', 1)
-        .attr('cx', function(d) { return x(d.date) })
-        .attr('cy', function(d) { return y(d.active_editors) });
+  .transition()
+  .duration(1000)
+  .style('opacity', 1)
+  .attr('cx', function(d) { return x(d['date']) })
+  .attr('cy', function(d) { return y(d[attribute]) });
 
-        circles
-        .transition()
-        .duration(2000)
-        .attr('cx', function(d) { return x(d.date) })
-        .attr('cy', function(d) { return y(d.active_editors) })
-        .attr('r', function() { return (3)})
-      // .attr('r', function() { return (hindin.new_articles.length <= 50) ? pointRadius : 0 })
-      .style('opacity', 1);
+  circles
+  .transition()
+  .duration(1000)
+  .attr('cx', function(d) { return x(d['date']) })
+  .attr('cy', function(d) { return y(d[attribute]) })
+  .attr('r', function() { return (3)})
+  .style('opacity', 1);
 
-      $('svg circle').tipsy({ 
-        gravity: 'w', 
-        html: true, 
-        title: function() {
-          var d = this.__data__;
-          var pDate = d.date;
-          return 'Date: ' + pDate.getDate() + "-" + pDate.getMonth() + "-" + pDate.getFullYear() + '<br>Value: ' + d.new_articles; 
-        }
-    });
+  $('svg circle').tipsy({ 
+    gravity: 'w', 
+    html: true, 
+    title: function() {
+      var d = this.__data__;
+      var pDate = d['date'];
+      return 'Date: ' + pDate.getDate() + "-" + pDate.getMonth() + "-" + pDate.getFullYear() + '<br>Value: ' + d[attribute]; 
     }
-  // parseData('hi');
+  });
 
-
-
-function filterLanguage(code) {
-  return (function(d) {return (d.language == code);});
 }
 
-// function parseData(code) {
-//     filteredData = data.filter(filterLanguage(code));
-//     for(var a=0; a<attributes.length; a++) {
-//       draw(filteredData, attributes[a]);
-//     }
-// }
+var date;
+function changeLanguage(code) {
 
-// function draw(data, div) {
+    filteredData = data.filter(filterLanguage(code));
+      if (flag==1) {
+    clear();
+    }
+
+    else {
+
+    flag = 1;
+    };
+    
+    for (var a = 0; a<attributes.length; a++) {
+      attribute = attributes[a];
+
+    filteredData.forEach(function(d) {
+
+      if (typeof(d['date']) != "object") {
+     
+        d['date'] = parseDate(d['date']);
+
+      }
+
+      d[attribute] = +d[attribute];
+
+    });
+
+    draw(filteredData, attribute);
+  }
+
+
+}
+
+
+function initialize() {
   
-//   data.forEach(function(d) {
-//     d.date = parseDate(d.date);
-//     d[div] = +d[div];
-//   });
+languages.forEach(function(d) {
+  d3.select("#search").append("option")
+  .attr("value", function() {return d.code})
+  .text(function() {return d.name});
 
-//   var line = d3.svg.line()
-//   .x(function(data) { return x(data.date); })
-//   .y(function(data) { return y(data[div]); });
-  
+  $('#search').select2("val", "hi");
+});
 
-// }
- 
+  $('#search').select2({placeholder: 'Select a language', width: '200px'});
+  $("#search").on("change", function(e){changeLanguage(e.val);});
 
-  // var svg = d3.select("#line").append("svg")
-  // .attr("width", width + margin.left + margin.right)
-  // .attr("height", height + margin.top + margin.bottom)
-  // .append("g")
-  // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // d3.csv("/indicwiki/data/data.csv", function(error, data) {
-  //   var hindi = data.filter(filterLanguage('ml'));
-  // // console.log(hindi);
-  // hindi.forEach(function(d) {
-  //   d.date = parseDate(d.date);
-  //   d.new_articles = +d.new_articles;
-  //   // console.log(d.date);
-  // });
-
-  
-  // x.domain(d3.extent(hindi, function(d) { return d.date; }));
-  // // x.domain(d3.extent(years, function(d) { return d; }));
-  // y.domain(d3.extent(hindi, function(d) { return d.new_articles; }));
-
-  // svg.append("g")
-  // .attr("class", "x axis")
-  // .attr("transform", "translate(0," + height + ")")
-  // .call(xAxis);
-
-  // svg.append("g")
-  // .attr("class", "y axis")
-  // .call(yAxis)
-  // .append("text")
-  // .attr("transform", "rotate(-90)")
-  // .attr("y", 6)
-  // .attr("dy", ".71em")
-  // .style("text-anchor", "end")
-  // .text("New Articles");
-
-  // svg.append("path")
-  // .datum(hindi)
-  // .attr("class", "line")
-  // .attr("d", line);
-
-
-  // if (!dataCirclesGroup) {
-  //   dataCirclesGroup = svg.append('svg:g');
-  // }
-
-  // filtered = hindi.filter(function(d) { if (d.new_articles!=0){return d};});
-
-  // var circles = dataCirclesGroup.selectAll('.data-point')
-  // .data(filtered);
-
-  // circles
-  // .enter()
-  // .append('svg:circle')
-  // .attr('class', 'data-point')
-  // .style('opacity', 1e-6)
-  // .attr('cx', function(d) { return x(d.date) })
-  // .attr('cy', function() { return y(0) })
-  // .attr('r', function() { return (2)})
-  //       // .attr('r', function() { return (hindi.length <= 50) ? pointRadius : 0 })
-  //       .transition()
-  //       .duration(2000)
-  //       .style('opacity', 1)
-  //       .attr('cx', function(d) { return x(d.date) })
-  //       .attr('cy', function(d) { return y(d.new_articles) });
-
-  //       circles
-  //       .transition()
-  //       .duration(2000)
-  //       .attr('cx', function(d) { return x(d.date) })
-  //       .attr('cy', function(d) { return y(d.new_articles) })
-  //       .attr('r', function() { return (3)})
-  //     // .attr('r', function() { return (hindin.new_articles.length <= 50) ? pointRadius : 0 })
-  //     .style('opacity', 1);
-
-  //     $('svg circle').tipsy({ 
-  //       gravity: 'w', 
-  //       html: true, 
-  //       title: function() {
-  //         var d = this.__data__;
-  //         var pDate = d.date;
-  //         return 'Date: ' + pDate.getDate() + "-" + pDate.getMonth() + "-" + pDate.getFullYear() + '<br>Value: ' + d.new_articles; 
-  //       }
-    //   });
-    // });
+}
